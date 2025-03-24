0163c8f111b65f697c727431c833838e017fc30b
guptadheerajj <174727182+guptadheerajj@users.noreply.github.com>
2025-03-24 10:02:32 +0530

    feat: make login module of the landing page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { aboutTab } from "./about.js";
 import { featuresTab } from "./features.js";
 import { contactTab } from "./contact.js";
 import { register } from "./register.js";
+import { login } from "./login.js";
 
 const tabContentMap = {
 	homeTab: homeTab.content,
@@ -11,12 +12,12 @@ const tabContentMap = {
 	featuresTab: featuresTab.content,
 	contactTab: contactTab.content,
 	register: register.content,
+	login: login.content,
 };
 
 const container = document.querySelector("#content");
 const tabs = document.querySelectorAll(".tab");
 const buttons = document.querySelectorAll(".btn");
-console.log(buttons);
 
 const tabListObj = {};
 tabs.forEach((tab) => {
@@ -39,8 +40,6 @@ function appendContent(target) {
 buttons.forEach((node) => {
 	node.addEventListener("click", () => {
 		const nodeName = node.dataset.name;
-		console.log(nodeName);
-
 		container.innerHTML = "";
 		container.appendChild(tabContentMap[nodeName]);
 	});
--- src/login.js
+++ src/login.js
@@ -1,27 +1,18 @@
-import googleSignUp from "./assets/sign_up_google_neutral.png";
-import userIcon from "./assets/user.svg";
+import googleSignIn from "./assets/signin_google_neutral.png";
 import emailIcon from "./assets/email.svg";
 import passwordIcon from "./assets/password.svg";
 
-export const register = (function () {
+export const login = (function () {
 	const content = document.createElement("div");
 	content.innerHTML = `
-		<div class="formTab">
-			<div class="form-message">
-				<h1>Hello, Friend!</h1>
-				<p>Enter your personal details and start journey with us</p>
-			</div>
+		<div class="formTab login">
 			<div class="form-container">
 				<form action="post">
-					<h1>Create Account</h1>
-					<a href="#"><img src="${googleSignUp}" alt="google-sign-up"></a>
-					<p>or use your email for registration:</p>
+					<h1>Sign in to CitySeva</h1>
+					<a href="#"><img src="${googleSignIn}" alt="google-sign-up"></a>
+					<p>or use your email account:</p>
 					<hr>
 					<div class="inputs">
-						<label for="user-name">
-							<img src="${userIcon}" alt="icon" height="28" width="28">
-							<input type="text" placeholder="Name" required autofocus>
-						</label>
 						<label for="user-name">
 							<img src="${emailIcon}" alt="icon" height="28" width="28">
 							<input type="email" placeholder="Email" required>
@@ -31,8 +22,12 @@ export const register = (function () {
 							<input type="password" placeholder="Password" required>
 						</label>
 					</div>
-					<button class="btn">SIGN UP</button>
+					<button class="btn">SIGN IN</button>
 				</form>
+			</div>
+			<div class="form-message">
+				<h1>Welcome Back!</h1>
+				<p>To keep connected with us please login with your personal information</p>
 			</div>
 		</div>
 	`;
